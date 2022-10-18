21b8c64645cfff5a7f5ed85c86db225a1a6c1674
LitoMore <litomore@gmail.com>
2022-10-19 01:29:19 +0800

    Apply cache-control to all requests

--- source/app.ts
+++ source/app.ts
@@ -3,6 +3,8 @@ import router from 'my-way';
 import {getSimpleIcon, getIconSvg} from './icon';
 
 const app = (request: VercelRequest, response: VercelResponse) => {
+	response.setHeader('Cache-Control', 'public, max-age=86400, s-maxage=31536000, stale-while-revalidate=604800');
+
 	if (request.method !== 'GET') {
 		return response.status(403).send({status: 403, message: 'forbidden'});
 	}
@@ -15,7 +17,7 @@ const app = (request: VercelRequest, response: VercelResponse) => {
 	if (icon) {
 		const iconSvg = getIconSvg(icon, color);
 		response.setHeader('Content-Type', 'image/svg+xml');
-		response.setHeader('Cache-Control', 'public, max-age=86400, s-maxage=31536000, stale-while-revalidate=604800');
+
 		return response.send(iconSvg);
 	}
 
