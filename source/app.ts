aa760447770b4ba90757c4b6fbb1a2cc8ed9b79a
LitoMore <litomore@gmail.com>
2022-10-26 13:08:53 +0800

    Transfer to Simple Icons

--- source/app.ts
+++ source/app.ts
@@ -25,7 +25,7 @@ const app = (request: VercelRequest, response: VercelResponse) => {
 	}
 
 	if (requestUrl === '/') {
-		return response.redirect(308, 'https://github.com/LitoMore/simple-icons-cdn');
+		return response.redirect(308, 'https://github.com/simple-icons/simple-icons-cdn');
 	}
 
 	return response.status(404).send({status: 404});
