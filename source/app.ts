98629acfa5328c93548f92eb8ab656a4fbadd8e3
LitoMore <litomore@gmail.com>
2023-09-12 14:21:18 +0800

    Support Node.js 20

--- source/app.ts
+++ source/app.ts
@@ -3,7 +3,10 @@ import router from 'my-way';
 import {getSimpleIcon, getIconSvg} from './icon';
 
 const app = (request: VercelRequest, response: VercelResponse) => {
-	response.setHeader('Cache-Control', 'public, max-age=86400, s-maxage=31536000, stale-while-revalidate=604800');
+	response.setHeader(
+		'Cache-Control',
+		'public, max-age=86400, s-maxage=31536000, stale-while-revalidate=604800',
+	);
 
 	if (request.method !== 'GET') {
 		return response.status(404).send({status: 404});
@@ -25,7 +28,10 @@ const app = (request: VercelRequest, response: VercelResponse) => {
 	}
 
 	if (requestUrl === '/') {
-		return response.redirect(308, 'https://github.com/LitoMore/simple-icons-cdn');
+		return response.redirect(
+			308,
+			'https://github.com/LitoMore/simple-icons-cdn',
+		);
 	}
 
 	return response.status(404).send({status: 404});
--- source/icon.ts
+++ source/icon.ts
@@ -9,12 +9,12 @@ export const getSimpleIcon = (slug?: string) => {
 
 	const normaizedSlug = slug
 		.toLowerCase()
-		.replace(/\+/g, 'plus')
-		.replace(/\./g, 'dot');
+		.replaceAll('+', 'plus')
+		.replaceAll('.', 'dot');
 
-	const iconKey = ('si'
-		+ normaizedSlug.charAt(0).toUpperCase()
-		+ normaizedSlug.slice(1)) as keyof typeof simpleIcons;
+	const iconKey = ('si' +
+		normaizedSlug.charAt(0).toUpperCase() +
+		normaizedSlug.slice(1)) as keyof typeof simpleIcons;
 
 	if (iconKey in simpleIcons) {
 		return simpleIcons[iconKey];
@@ -23,12 +23,19 @@ export const getSimpleIcon = (slug?: string) => {
 	return null;
 };
 
-export const getIconSvg = (icon: SimpleIcon, color = '', darkModeColor = '') => {
+export const getIconSvg = (
+	icon: SimpleIcon,
+	color = '',
+	darkModeColor = '',
+) => {
 	const hex = normalizeColor(color) || `#${icon.hex}`;
 	const darkModeHex = normalizeColor(darkModeColor);
 
 	if (darkModeColor && hex !== darkModeHex) {
-		return icon.svg.replace('<path ', `<style>path{fill:${hex}} @media (prefers-color-scheme:dark){path{fill:${darkModeHex}}}</style><path `);
+		return icon.svg.replace(
+			'<path ',
+			`<style>path{fill:${hex}} @media (prefers-color-scheme:dark){path{fill:${darkModeHex}}}</style><path `,
+		);
 	}
 
 	return icon.svg.replace('<svg ', `<svg fill="${hex}" `);
