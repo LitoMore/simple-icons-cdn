b0ce773d7b0ef05aeb53dadf40025393f6e5c752
LitoMore <litomore@gmail.com>
2024-10-06 13:01:24 +0800

    Use Deno built-in test utilities

--- test/viewbox.test.ts
+++ test/viewbox.test.ts
@@ -1,54 +1,42 @@
 import * as si from 'simple-icons';
 import { getIconSvg } from '../source/icon.ts';
 
-const checkAutoViewboxPath = (icon: si.SimpleIcon) => {
-	const start = performance.now();
-	try {
+const icons = new Map<string, si.SimpleIcon>(
+	Object.entries(si).map((
+		[k, v],
+	) => [k.slice(2).toLowerCase(), v as si.SimpleIcon]),
+);
+
+Deno.test('test auto-viewbox', () => {
+	const result: Array<{ title: string; time: number; fail: boolean }> = [];
+	for (const [, icon] of icons) {
+		const start = performance.now();
 		const iconSvg = getIconSvg(icon, { viewbox: 'auto' });
 		const end = performance.now();
-		if (iconSvg) {
-			return { title: icon.title, time: end - start };
-		}
-		throw new Error('Path is empty');
-	} catch (error) {
-		const end = performance.now();
-		console.error(
-			`Error in icon: ${icon.title}: ${
-				error instanceof Error ? error.message : 'Unknown error'
-			}`,
-		);
-		return { title: icon.title, time: end - start, fail: true };
+		result.push({ title: icon.title, time: end - start, fail: !iconSvg });
 	}
-};
 
-const result = Object.values(si).map((icon) =>
-	checkAutoViewboxPath(icon as si.SimpleIcon)
-);
-const iconsFailed = result.filter((r) => r.fail);
-
-console.log('Top 10 slow icons:');
-console.table(
-	result.sort((a, b) => b.time - a.time).slice(0, 10).map((x) => ({
-		title: x.title,
-		['time (ms)']: Number(x.time.toFixed(3)),
-	})),
-);
-
-const iconArgs = Deno.args;
-if (iconArgs.length > 0) {
+	const iconsFailed = result.filter((r) => r.fail);
+	console.log('Top 10 slow icons:');
 	console.table(
-		result.filter((r) => iconArgs.includes(r.title)).map((x) => ({
+		result.sort((a, b) => b.time - a.time).slice(0, 10).map((x) => ({
 			title: x.title,
 			['time (ms)']: Number(x.time.toFixed(3)),
 		})),
 	);
-}
 
-if (iconsFailed.length > 0) {
-	console.log(`Failed icons:`);
-	console.table(iconsFailed);
-	Deno.exit(1);
-} else {
-	console.log('All icons passed');
-	Deno.exit(0);
-}
+	const iconArgs = new Set(Deno.args);
+	if (iconArgs.size > 0) {
+		console.table(
+			result.filter((r) => iconArgs.has(r.title)).map((x) => ({
+				title: x.title,
+				['time (ms)']: Number(x.time.toFixed(3)),
+			})),
+		);
+	}
+
+	if (iconsFailed.length > 0) {
+		console.table(iconsFailed);
+		throw new Error(`${iconsFailed.length} icons failed`);
+	}
+});
